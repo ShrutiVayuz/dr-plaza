6b286ad1f1ecec27c349ceff03f74c9adc543d00
ShrutiVayuz <Shruti.Agarwal@vayuz.com>
2021-01-29 14:57:54 +0530

    admin

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const ATLAS_DB_URI = 'mongodb+srv://User1:user1@cluster0.gxylu.mongodb.net/DoctorsAppDB?retryWrites=true&w=majority';
+const ATLAS_DB_URI = 'mongodb+srv://shruti:shruti@cluster0.v08jw.mongodb.net/<dr-plaza?retryWrites=true&w=majority';
 mongoose.connect(process.env.MONGODB_URI || ATLAS_DB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
